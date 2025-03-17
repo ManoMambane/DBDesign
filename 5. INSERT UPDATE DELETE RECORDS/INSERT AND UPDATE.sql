05894ef861b3c205b74be7c31ba4f9c488689069
Mano Mambane <manomambane05@gmail.com>
2025-03-17 17:22:03 +0200

    DB

--- 5. INSERT UPDATE DELETE RECORDS/INSERT AND UPDATE.sql
+++ 5. INSERT UPDATE DELETE RECORDS/INSERT AND UPDATE.sql
@@ -2,11 +2,17 @@ USE CANDYSTORE;
 
 SELECT * FROM EMPLOYEES;
 
+-- inserted values
 insert into employees
 values (7, 'Charles', 'Munger', '2020-03-15', 'Clerk', null),
 (8, 'William', 'Gates', '2020-03-15', 'Clerk', null);
 
+insert into employees
+values
+(9, 'Mano', 'Mambane', '1998-05-03', 'Manager', 5.0),
+(10, 'Ntivo', 'Mambane', '2025-01-07', 'CEO', 9.7 );
 
+-- updated values
 update employees
 set avg_customer_rating = 4.8
 where employee_id = 1;
@@ -38,3 +44,14 @@ where employee_id = 7;
 update employees
 set avg_customer_rating = 5.0
 where employee_id = 8;
+
+update employees
+set first_name = 'Zanca'
+where employee_id = 10;
+
+update employee
+set last_name = "Ntivo"
+where employee_id = 10;
+
+
+
